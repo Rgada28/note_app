eed0fa929829b1d244446b5078b170e4d92f979e
dickyrey <dicky.rey97@gmail.com>
2020-07-01 11:19:31 +0700

    Flutter Firebase & DDD Course [6] - Firebase Auth Setup & Facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,11 +1,18 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
+import 'package:noteapp/domain/core/errors.dart';
 import 'package:noteapp/domain/core/failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Thorws [UnecpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
 
   bool isValid() => value.isRight();
 
