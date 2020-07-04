f0d1c1b6c4b30a083f5e66fbe38ad2a706772b09
dickyrey <dicky.rey97@gmail.com>
2020-07-05 01:45:50 +0700

    Flutter Firebase & DDD Course [15] - Note Watcher & Actor Blocs

--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -9,6 +9,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:noteapp/infrastrcuture/auth/firebase_auth_facade.dart';
 import 'package:noteapp/domain/auth/i_auth_facade.dart';
+import 'package:noteapp/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:noteapp/domain/notes/i_note_repository.dart';
 import 'package:noteapp/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:noteapp/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -21,6 +23,8 @@ void $initGetIt(GetIt g, {String environment}) {
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerFactory<NoteWatcherBloc>(
+      () => NoteWatcherBloc(g<INoteRepository>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
   g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
 }
