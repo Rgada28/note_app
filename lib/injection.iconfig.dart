3879adc5d031dc076d4ea71cf818cc8e1a0b17f6
dickyrey <dicky.rey97@gmail.com>
2020-07-07 17:02:33 +0700

    Flutter Firebase & DDD Course [16] - Watching Notes on Firestore

--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -9,8 +9,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:noteapp/infrastrcuture/auth/firebase_auth_facade.dart';
 import 'package:noteapp/domain/auth/i_auth_facade.dart';
-import 'package:noteapp/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:noteapp/infrastrcuture/notes/note_repository.dart';
 import 'package:noteapp/domain/notes/i_note_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:noteapp/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:noteapp/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:noteapp/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -23,6 +25,8 @@ void $initGetIt(GetIt g, {String environment}) {
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerLazySingleton<INoteRepository>(
+      () => NoteRepository(g<Firestore>()));
   g.registerFactory<NoteWatcherBloc>(
       () => NoteWatcherBloc(g<INoteRepository>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
